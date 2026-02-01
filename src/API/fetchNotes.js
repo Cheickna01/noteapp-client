f26b55c3f4fd75ac4027f344f07a5bbc8532a7b6
Cheickna01 <diabyhamala0@gmail.com>
2026-02-01 12:53:36 +0100

    changes

--- src/API/fetchNotes.js
+++ src/API/fetchNotes.js
@@ -21,6 +21,6 @@ export const fetchNotes = (setNotes, filters, setAllTags) => {
     })
     .catch((e) => {
       console.log(e);
-      toast.error("Veuillez réessayer!");
+      toast.error(e);
     });
 };
